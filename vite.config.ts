1faeed1e2608e8cda7b7f1e876584df9454b01c1
Satyanarayana2417 <snsnarayanac@gmail.com>
2026-06-12 16:50:16 +0530

    qie

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteTsconfigPaths(),
     react(),
